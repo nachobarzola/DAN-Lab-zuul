99d00e35ebd8aaeed7088df66539d36c4a018f53
nachobarzola <54648366+nachobarzola@users.noreply.github.com>
2021-08-02 20:41:46 -0300

    Tira otro error pero sigue sin andar

--- src/main/java/damms/zuul/ZuulApplication.java
+++ src/main/java/damms/zuul/ZuulApplication.java
@@ -2,14 +2,23 @@ package damms.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import damms.zuul.filter.PreFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class ZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
 	}
 
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
 }
